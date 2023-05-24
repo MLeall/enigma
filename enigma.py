1c42fd25ba7ed8c5e74a7828cf2d68a0d0f1f989
Matheus Leal Leone <matheusleal.leone@gmail.com>
2023-05-24 10:44:03 -0500

    initial concept of enigma class

---
+++ enigma.py
@@ -0,0 +1,7 @@
+class Enigma:
+    """
+    Enigma class should have the rotors logic, encryption and decryption logic and a management method to ensure consistency.
+    """
+    def __init__(self) -> None:
+        pass
+
